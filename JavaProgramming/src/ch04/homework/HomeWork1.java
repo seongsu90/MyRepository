b91241324f723330132979f35bc09b2e8fe2da8d
seongsu90 <ssbagss@hanmail.net>
2016-08-18 21:43:05 +0900

    HomeWork1,2 추가

--- JavaProgramming/src/ch04/homework/HomeWork1.java
+++ JavaProgramming/src/ch04/homework/HomeWork1.java
@@ -4,7 +4,16 @@ public class HomeWork1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+				
+		for(int i=0; i<100;i++) // 주사위 100번을 던진다.
+		{
+			int diceA = (int) (Math.random()*6)+1; // 첫번째 랜덤 주사위
+			int diceB = (int) (Math.random()*6)+1;// 두번째 랜덤 주사위
+			if(diceA+diceB == 10) // 두 주사위의 합이 10일때 true로서 if문의 출력문이 진행
+			{
+				System.out.println("("+diceA+ ","+diceB+")"); // 두 주사위의 합이 10인 주사위눈의 결과문 출력
+			}
+		}
 	}
 
 }
--- JavaProgramming/src/ch04/homework/HomeWork2.java
+++ JavaProgramming/src/ch04/homework/HomeWork2.java
@@ -4,7 +4,19 @@ public class HomeWork2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+						
+		for(int x=1;x<=10;x++) // 1<= x <= 10 를 나타내는 x의 for문
+		{
+			for(int y=1;y<=10;y++) // 1<=y<=10를 나타내는 y의 for문
+			{
+				if(4*x+5*y==60) // if조건문에서 문제의 방정식을 조건으로 두고 60의 조건에 맞을때 (x,y)를 출력
+				{
+					System.out.println("("+x+","+y+")"); // 조건문에 해당되는 값을 출력
+				}
+			}
+		}
+		
+		
 	}
 
 }
