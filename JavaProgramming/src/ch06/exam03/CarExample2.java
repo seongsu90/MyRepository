9d61cae6940b23d2c26828f358f1ab2ae5316d04
seongsu90 <ssbagss@hanmail.net>
2016-08-23 18:55:23 +0900

    6.6 예제

---
+++ JavaProgramming/src/ch06/exam03/CarExample2.java
@@ -0,0 +1,20 @@
+package ch06.exam03;
+
+public class CarExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car2 myCar = new Car2();
+		
+		System.out.println("제작회사: " + myCar.company);
+		System.out.println("모델명: " + myCar.model);
+		System.out.println("색깔: " + myCar.color);
+		System.out.println("최고속도: " + myCar.maxSpeed);
+		System.out.println("현재속도: " + myCar.speed);
+		
+		myCar.speed =60;
+		System.out.println("수정된 속도: "+myCar.speed);
+	}
+
+}
---
+++ JavaProgramming/src/ch06/exam03/FieldInitValue.java
@@ -0,0 +1,20 @@
+package ch06.exam03;
+
+public class FieldInitValue {
+	byte byteField;
+	short shortField;
+	int intField;
+	long longField;
+	
+	boolean booleanField;
+	char charField;
+	
+	float floatField;
+	double doubleField;
+	
+	int[] arrField;
+	String referenceField;
+	
+	
+	
+}
