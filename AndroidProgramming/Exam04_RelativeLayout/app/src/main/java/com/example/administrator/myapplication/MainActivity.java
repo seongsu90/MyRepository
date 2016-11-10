7b98f92ca96ec50656a83fdb899a38ba346688e2
seongsu90 <ssbagss@hanmail.net>
2016-11-10 10:06:54 +0900

    ad

---
+++ AndroidProgramming/Exam04_RelativeLayout/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //R은 res 폴더를 의미 (Resource 참조)
+    }
+}
